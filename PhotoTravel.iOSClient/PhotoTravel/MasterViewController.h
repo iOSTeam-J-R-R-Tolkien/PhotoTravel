de67f9d89db3405831f7b726e32e9ab7f4e5146e
stiliankanchev <peshopeshev@Peshos-Mac-Pro.local>
2014-11-03 01:50:30 +0200

    Blank iOS Application

---
+++ PhotoTravel.iOSClient/PhotoTravel/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  PhotoTravel
+//
+//  Created by Pesho Peshev on 11/3/14.
+//  Copyright (c) 2014 PhotoTravel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
