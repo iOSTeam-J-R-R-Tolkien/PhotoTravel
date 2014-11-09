74c1aee4e4fd8edc1e9e66d223a3b0a6bbbc8063
NoMoree <vindicator@Vindicators-Mac.local>
2014-11-10 01:48:57 +0200

    add LandmarkUITableViewCell.xib + class

---
+++ PhotoTravel.iOS.Client/PhotoTravel.iOS.Client/ViewControllers/LandmarkUITableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LandmarkUITableViewCell.h
+//  PhotoTravel.iOS.Client
+//
+//  Created by Vindicator on 11/10/14.
+//  Copyright (c) 2014 Vindicator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandmarkUITableViewCell : UITableViewCell
+
+@end
---
+++ PhotoTravel.iOS.Client/PhotoTravel.iOS.Client/ViewControllers/LandmarkUITableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  LandmarkUITableViewCell.m
+//  PhotoTravel.iOS.Client
+//
+//  Created by Vindicator on 11/10/14.
+//  Copyright (c) 2014 Vindicator. All rights reserved.
+//
+
+#import "LandmarkUITableViewCell.h"
+
+@implementation LandmarkUITableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
